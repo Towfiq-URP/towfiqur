acab7bdcc35756c7c67ef808ce8acd5775931393
Towfiq-URP <2007053.towfiq@gmail.com>
2026-02-12 05:53:07 +0000

    Deploying to gh-pages from @ Towfiq-URP/towfiqur@94a7432d1883f862081d50a8a24a8a711caaf3dc 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "/assets/libs/photoswipe-lightbox/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "/assets/libs/photoswipe/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "/towfiqur/assets/libs/photoswipe-lightbox/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "/towfiqur/assets/libs/photoswipe/photoswipe.esm.min.js";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
